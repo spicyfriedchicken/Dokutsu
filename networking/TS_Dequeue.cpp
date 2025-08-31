f67a68d3a194caccc8dc334651d0ab7fd6be1ef0
spicyfriedchicken <214891781+spicyfriedchicken@users.noreply.github.com>
2025-08-31 18:44:48 -0400

    tsDeq

---
+++ networking/TS_Dequeue.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <stdexcept>
+#include <utility>
+#include <shared_mutex>
+
+template <typename T>
+class Deque {
+private:
+    struct Node {
+        T value;
+        Node* next;
+        Node* prev;
+        explicit Node(const T& val) : value(val), prev(nullptr), next(nullptr) {}
+        Node() : value(T()), next(nullptr), prev(nullptr) {}
+
+        Node(const Node&) = delete;
+        Node& operator=(const Node&) = delete;
+        Node(Node&&) = delete;
+        Node& operator=(Node&&) = delete;
+    };
+
+    Node* head_;
+    Node* tail_;
+    size_t size_;
+    mutable std::shared_mutex mut_;
+
+    template<typename... Args>
+    void emplace_front(Args&&... args) {
+        Node* temp = head_->next;
+        Node* newNode = new Node(T(std::forward<Args>(args)...));
+        head_->next = newNode;
+        temp->prev = newNode;
+        newNode->next = temp;
+        newNode->prev = head_;
+        ++size_;
+    }
+
+    template<typename... Args>
+    void emplace_back(Args&&... args) {
+        Node* temp = tail_->prev;
+        Node* newNode = new Node(T(std::forward<Args>(args)...));
+        tail_->prev = newNode;
+        temp->next = newNode;
+        newNode->next = tail_;
+        newNode->prev = temp;
+        ++size_;
+    }
+
+    void pop_front() {
+        if (head_->next == tail_) return;
+        Node* victim = head_->next;
+        head_->next = victim->next;
+        victim->next->prev = head_;
+        delete victim;
+        --size_;
+    }
+
+    void pop_back() {
+        if (tail_->prev == head_) return;
+        Node* victim = tail_->prev;
+        tail_->prev = victim->prev;
+        victim->prev->next = tail_;
+        delete victim;
+        --size_;
+    }
+
+public:
+    Deque() : head_(new Node()), tail_(new Node()), size_(0) {
+        head_->next = tail_;
+        tail_->prev = head_;
+    }
+
+    Deque(const Deque&) = delete;
+    Deque& operator=(const Deque&) = delete;
+
+    Deque(Deque&& other) noexcept {
+        std::unique_lock lk_other(other.mut_);
+        head_ = std::exchange(other.head_, nullptr);
+        tail_ = std::exchange(other.tail_, nullptr);
+        size_ = std::exchange(other.size_, 0);
+    }
+
+    Deque& operator=(Deque&& other) noexcept {
+        if (this != &other) {
+            std::scoped_lock lk(mut_, other.mut_);
+            while (head_ && head_->next && head_->next != tail_) pop_front();
+            delete head_;
+            delete tail_;
+            head_ = std::exchange(other.head_, nullptr);
+            tail_ = std::exchange(other.tail_, nullptr);
+            size_ = std::exchange(other.size_, 0);
+        }
+        return *this;
+    }
+
+    void push_front(const T& value) {
+        std::unique_lock lk(mut_);
+        emplace_front(value);
+    }
+
+    void push_back(const T& value) {
+        std::unique_lock lk (mut_);
+        emplace_back(value);
+    }
+
+    [[nodiscard]] T& front() {
+        std::shared_lock lk (mut_);
+        if (head_->next != tail_) throw std::runtime_error("Deque is empty.");
+        return head_->next->value;
+    }
+
+    [[nodiscard]] const T& front() const {
+        std::shared_lock lk (mut_);
+        if (head_->next != tail_) throw std::runtime_error("Deque is empty.");
+        return head_->next->value;
+    }
+
+    [[nodiscard]] T& back() {
+        std::shared_lock lk (mut_);
+        if (head_->next == tail_) throw std::runtime_error("Deque is empty.");
+        return tail_->prev->value;
+    }
+
+    [[nodiscard]] const T& back() const {
+        std::shared_lock lk (mut_);
+        if (head_->next == tail_) throw std::runtime_error("Deque is empty.");
+        return tail_->prev->value;
+    }
+
+    void clear() {
+        std::unique_lock lk (mut_);
+        while (head_->next != tail_) {  // don't call empty()
+            pop_front();
+        }
+    }
+
+    [[nodiscard]] bool empty() const noexcept { std::shared_lock lk (mut_); return size_ == 0; }
+    [[nodiscard]] size_t get_size() const noexcept { std::shared_lock lk (mut_); return size_; }
+
+    ~Deque() {
+        while (head_ && head_->next && head_->next != tail_) pop_front();
+        if (head_) delete head_;
+        if (tail_) delete tail_;
+    }
+};
--- networking/message.hpp
+++ networking/message.hpp
@@ -1,5 +1,4 @@
-#ifndef COMMON_H
-#define COMMON_H
+#pragma once
 
 #include <vector>
 #include <cstdint>
@@ -7,8 +6,20 @@
 #include <memory>
 #include <type_traits>
 #include <cstring>
+#include <limits>
 
-namespace net {
+static inline uint16_t be16(uint16_t v) {
+    return htons(v);
+}
+static inline uint32_t be32(uint32_t v) {
+    return htonl(v);
+}
+static inline uint16_t from_be16(uint16_t v) {
+    return ntohs(v);
+}
+static inline uint32_t from_be32(uint32_t v) {
+    return ntohl(v);
+}
 
 template <typename T>
 struct messageHeader {
@@ -21,14 +32,9 @@ struct message {
     messageHeader<T> header_;
     std::vector<uint8_t> data_;
 
-    friend std::ostream& operator<<(std::ostream& os, const message<T>& msg) {
-        os << "ID: " << int(msg.header_.id) << " Size: " << msg.header_.size;
-        return os;
-    }
-
     template <typename DataType>
     friend message<T>& operator<<(message<T>& msg, const DataType& data) {
-        static_assert(std::is_standard_layout<DataType>::value, "DataType must be standard layout");
+        static_assert(std::is_trivially_copyable<DataType>::value, "DataType must be standard layout");
         size_t i = msg.data_.size();
         msg.data_.resize(i + sizeof(DataType));
         std::memcpy(msg.data_.data() + i, &data, sizeof(DataType));
@@ -38,12 +44,20 @@ struct message {
 
     template <typename DataType>
     friend message<T>& operator>>(message<T>& msg, DataType& data) {
-        static_assert(std::is_standard_layout<DataType>::value, "DataType must be standard layout");
+        static_assert(std::is_trivially_copyable<DataType>::value, "DataType must be trivially copyable");
         size_t i = msg.data_.size() - sizeof(DataType);
         std::memcpy(&data, msg.data_.data() + i, sizeof(DataType));
         msg.data_.resize(i);
         msg.header_.size = msg.data_.size();
         return msg;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const message<T>& msg) {
+        using U = std::underlying_type_t<T>;
+
+        os << "ID: " << static_cast<int>(static_cast<U>(msg.header_.id))
+           << "Size: " << msg.header_.size;
+        return os;
     }
 
     size_t size() const { return sizeof(messageHeader<T>) + data_.size(); }
@@ -62,7 +76,3 @@ struct owned_message {
         return os;
     }
 };
-
-} // namespace net
-
-#endif // COMMON_H
